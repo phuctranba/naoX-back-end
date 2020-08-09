66845174587ebbf62ab30ff6d1cdbd0cb934bada
TranBaPhuc <Tranbaphuc@hou.edu.vn>
2020-08-09 21:24:40 +0700

    delete from master

--- exporters.py
+++
@@ -1,8 +0,0 @@
-from scrapy.exporters import CsvItemExporter
-
-
-class Utf8CsvItemExporter(CsvItemExporter):
-
-    def __init__(self, file, **kwargs):
-        super(CsvItemExporter, self).__init__(
-            file, ensure_ascii=False, **kwargs)
